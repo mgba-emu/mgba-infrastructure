c5034aea7004b7efc15b7db98968e9e45d7e5ee2
Vicki Pfau <vi@endrift.com>
2020-07-05 23:56:25 +0000

    Updates again

--- buildbot/s3-upload.sh
+++ buildbot/s3-upload.sh
@@ -1,37 +1,15 @@
 #!/bin/bash
-source ~/master/.aws-cred
 prefix=$1
 file=$2
 linkName=$3
 fileName=`basename ${file}`
 bucket=mgba
-resource="/${bucket}/${prefix}${fileName}"
-contentType="application/x-compressed"
-dateValue=`date -R`
+contentType="${4:-application/x-compressed}"
 storageClass=STANDARD
+acl="${5:-public-read}"
 
-stringToSign="PUT\n\n${contentType}\n${dateValue}\nx-amz-acl:public-read\nx-amz-storage-class:${storageClass}\n${resource}"
-echo $stringToSign
-echo $storageClass
-signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
-curl -X PUT -T "${file}" --retry 3 \
-  -H "Host: ${bucket}.s3.amazonaws.com" \
-  -H "Date: ${dateValue}" \
-  -H "Content-Type: ${contentType}" \
-  -H "Authorization: AWS ${s3Key}:${signature}" \
-  -H "x-amz-acl: public-read" \
-  -H "x-amz-storage-class: ${storageClass}" \
-  https://${bucket}.s3.amazonaws.com/${prefix}${fileName} || exit $?
+aws s3 cp --quiet --acl ${acl} --storage-class ${storageClass} --content-type ${contentType} "${file}" "s3://${bucket}/${prefix}${fileName}"
 
 if [ -n "$linkName" ]; then
-	stringToSign="PUT\n\n\n${dateValue}\nx-amz-acl:public-read\nx-amz-copy-source:${resource}\nx-amz-storage-class:STANDARD\n/${bucket}/${linkName}"
-	signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
-	curl -X PUT --retry 3 \
-	  -H "Host: ${bucket}.s3.amazonaws.com" \
-	  -H "Date: ${dateValue}" \
-	  -H "Authorization: AWS ${s3Key}:${signature}" \
-	  -H "x-amz-acl: public-read" \
-	  -H "x-amz-copy-source: ${resource}" \
-	  -H "x-amz-storage-class: ${storageClass}" \
-	  https://${bucket}.s3.amazonaws.com/${linkName} || exit $?
+	aws s3 cp --quiet --acl ${acl} --storage-class ${storageClass} --content-type ${contentType} "s3://${bucket}/${prefix}${fileName}" "s3://${bucket}/${linkName}"
 fi
